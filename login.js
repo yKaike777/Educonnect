7eea244633e5b49b0a67ce7baaba3a624ef3dafd
yKaike777 <kaike.barros@aluno.senai.br>
2025-09-20 18:54:53 -0300

    Salvando dados com SessionStorage

--- login.js
+++ login.js
@@ -1,13 +1,22 @@
 function entrar() {
     const radio = document.querySelector('input[name="aluno-professor"]:checked')
-    
-    if (radio.value === "aluno") {
-        alert("Entrando como Aluno")
-        window.location.href = "aluno.html"
-    } else if (radio.value === "professor") {
-        alert("Entrando como Professor")
-        window.location.href = "professor.html"
-    } else {
-        alert("Selecione uma opção")
+    const nome = document.getElementById("nome").value
+
+    if (nome){
+
+        if (radio.value === "aluno") {
+            alert(`Bem-vindo, ${nome}! Entrando como Aluno...`)
+            sessionStorage.clear()
+            sessionStorage.setItem("nome", nome)
+            window.location.href = "aluno.html"
+        } else if (radio.value === "professor") {
+            alert(`Bem-vindo, ${nome}! Entrando como Professor...`)
+            sessionStorage.clear()
+            sessionStorage.setItem("nome", nome)
+            window.location.href = "professor.html"
+        }
+    }else{
+        alert("Preencha o nome")
     }
+
 }
---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("nome").innerText = sessionStorage.getItem("nome").toUpperCase()
+
+let database = {
+    "turmas" : [
+        {"turmaID": 1, "nome": "Turma A"},
+        {"turmaID": 2, "nome": "Turma B"},
+        {"turmaID": 3, "nome": "Turma C"},
+        {"turmaID": 4, "nome": "Turma D"}
+    ],
+    "alunos": [
+        {"id": 1, "nome": "Ana", "notas": [], "turma": 1},
+        {"id": 2, "nome": "Beatriz", "notas": [], "turma": 1},
+        {"id": 3, "nome": "Carlos", "notas": [], "turma": 1},
+        {"id": 4, "nome": "Daniel", "notas": [], "turma": 1},
+        {"id": 5, "nome": "Eduardo", "notas": [], "turma": 2},
+        {"id": 6, "nome": "Fernanda", "notas": [], "turma": 2},
+        {"id": 7, "nome": "Gabriel", "notas": [], "turma": 2},
+        {"id": 8, "nome": "Helena", "notas": [], "turma": 2},
+        {"id": 9, "nome": "Isabela", "notas": [], "turma": 3},
+        {"id": 10, "nome": "João", "notas": [], "turma": 3},
+        {"id": 11, "nome": "Karla", "notas": [], "turma": 3},
+        {"id": 12, "nome": "Lucas", "notas": [], "turma": 3},
+        {"id": 13, "nome": "Marcos", "notas": [], "turma": 4},
+        {"id": 14, "nome": "Natália", "notas": [], "turma": 4},
+        {"id": 15, "nome": "Olivia", "notas": [], "turma": 4},
+        {"id": 16, "nome": "Pedro", "notas": [], "turma": 4},
+    ]
+}
