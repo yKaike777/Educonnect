9a43257be460881ad407c284085388890428f70e
yKaike777 <kaike.barros@aluno.senai.br>
2025-09-21 21:16:04 -0300

    Tabela de Alunos

--- professor.js
+++ professor.js
@@ -4,10 +4,12 @@ cardsTurma.forEach((card) => {
     const ID = card.getAttribute('id')
     const turma = database.turmas.find(t => t.turmaID == ID)
 
+    // Atualiza o nome da turma no card
     if (turma){
         card.querySelector('h2').textContent = "Turma " + turma.nome
     }
 
+    // Adiciona o evento de clique para redirecionar para a página da turma
     card.addEventListener('click', () => {
         window.location.href = `turma.html?turmaID=${ID}`;
     })
--- turma.js
+++ turma.js
@@ -1,6 +1,50 @@
 const params = new URLSearchParams(window.location.search);
 const turmaID = params.get('turmaID');
 
+// Acha o nome da turma com base no ID
 const turmaName = database.turmas.find(turma => turma.turmaID == turmaID)?.nome || 'Turma Desconhecida';
 
 document.getElementById('turma').textContent = `Turma ${turmaName}`;
+
+// Filtra os alunos que pertencem à turma atual
+const alunos = database.alunos.filter(aluno => aluno.turmaID == turmaID);
+const alunosNome = alunos.map(aluno => aluno.nome);
+
+// Atualiza a quantidade de alunos
+const qntdAlunos = document.getElementById('qntd-alunos');
+qntdAlunos.textContent = `Quantidade de Alunos: ${alunos.length}`;
+
+alunos.forEach((aluno) => {
+    // Adiciona uma nova linha para cada aluno
+    const tabela = document.querySelector('tbody');
+    const linha = document.createElement('tr');
+
+    linha.classList.add('aluno');
+    linha.setAttribute('id', aluno.id);
+
+    linha.innerHTML = `
+                    <tr class="aluno" id="1">
+                        <td class="num-chamada">10</td>
+                        <td class="nome-aluno">nome do aluno</td>
+                        <td class="nota-primeiro-tri">6,0 <i class="fa-solid fa-pen-to-square"></i></td>
+                        <td class="nota-segundo-tri">6,0 <i class="fa-solid fa-pen-to-square"></i></td>
+                        <td class="nota-terceiro-tri">6,0 <i class="fa-solid fa-pen-to-square"></i></td>
+                        <td class="presenca">Sim</td>                      
+                   </tr>
+                    `;
+
+    tabela.appendChild(linha);
+});
+
+// Preenche os nomes dos alunos e números de chamada
+const nomeTabela = document.querySelectorAll('.nome-aluno');
+
+nomeTabela.forEach((aluno, index) => {
+    aluno.textContent = alunosNome[index];
+
+    const numChamada = document.querySelectorAll('.num-chamada');
+    numChamada[index].textContent = index + 1;
+});
+
+const nomeProfessor = document.getElementById('professor');
+nomeProfessor.textContent = sessionStorage.getItem('nome');
